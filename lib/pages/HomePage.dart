f27cae7bcff6a9cb3345b0797a8a43a7d8393ff6
Aykut Asil <aykuttasil@gmail.com>
2019-09-08 23:58:09 +0300

    update

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -1,5 +1,6 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_playground/pages/AboutPage.dart';
+import 'package:flutter_playground/data/AppDio.dart';
 import 'package:flutter_playground/pages/AnimatedBoxPage.dart';
 import 'package:flutter_playground/pages/NavigateWithArgumentsPage.dart';
 import 'package:flutter_playground/pages/PageRouteBuilder.dart';
@@ -19,54 +20,69 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            RaisedButton(
-              child: Text("Arguments Page"),
-              onPressed: () => {
-                Navigator.pushNamed(
-                  context,
-                  NavigateWithArgumentsPage.routeName,
-                  arguments: MyArguments("Sample Title", "Sample message"),
-                )
-              },
-            ),
-            RaisedButton(
-              child: Text("Arguments Page 123"),
-              onPressed: () => {
-                Navigator.push(context, MaterialPageRoute(builder: (context) {
-                  return Center(
-                    child: Container(
-                      decoration: BoxDecoration(color: Colors.yellow),
-                      constraints: BoxConstraints.expand(),
-                      child: Text("Aykut AaAAsil"),
-                    ),
-                  );
-                }))
-              },
-            ),
-            RaisedButton(
-              child: Text("AnimatedBoxPage"),
-              onPressed: () => {
-                Navigator.push(context, MaterialPageRoute(builder: (context) {
-                  return AnimatedBoxPage();
-                }))
-              },
-            ),
-            RaisedButton(
-              child: Text("PageRouteBuilderPage"),
-              onPressed: () => {
-                Navigator.of(context).push(_createRouteToPageRouteBuilderPage())
-              },
-            ),
-          ],
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            stops: [0.0, 0.8],
+            colors: [
+              Colors.cyan,
+              Colors.white,
+            ],
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              RaisedButton(
+                child: Text("Arguments Page"),
+                onPressed: () => {
+                  Navigator.pushNamed(
+                    context,
+                    NavigateWithArgumentsPage.routeName,
+                    arguments: MyArguments("Sample Title", "Sample message"),
+                  )
+                },
+              ),
+              RaisedButton(
+                child: Text("Arguments Page 123"),
+                onPressed: () => {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) {
+                    return Center(
+                      child: Container(
+                        decoration: BoxDecoration(color: Colors.yellow),
+                        constraints: BoxConstraints.expand(),
+                        child: Text("Aykut AaAAsil"),
+                      ),
+                    );
+                  }))
+                },
+              ),
+              RaisedButton(
+                child: Text("AnimatedBoxPage"),
+                onPressed: () => {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) {
+                    return AnimatedBoxPage();
+                  }))
+                },
+              ),
+              RaisedButton(
+                child: Text("PageRouteBuilderPage"),
+                onPressed: () => {
+                  Navigator.of(context)
+                      .push(_createRouteToPageRouteBuilderPage())
+                },
+              ),
+            ],
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          Navigator.pushNamed(context, AboutPage.routeName);
+          getHttp();
+          // Navigator.pushNamed(context, AboutPage.routeName);
         },
         tooltip: 'Increment',
         child: Icon(Icons.add),
@@ -83,7 +99,8 @@ class _MyHomePageState extends State<MyHomePage> {
         var end = Offset.zero;
         var curve = Curves.bounceInOut;
 
-        var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+        var tween =
+            Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
 
         return SlideTransition(
           position: animation.drive(tween),
@@ -91,5 +108,14 @@ class _MyHomePageState extends State<MyHomePage> {
         );
       },
     );
+  }
+}
+
+void getHttp() async {
+  try {
+    Response response = await AppDio.get().get("http://www.google.com");
+    print(response);
+  } catch (e) {
+    print(e);
   }
 }
--- lib/theme/AppTheme.dart
+++ lib/theme/AppTheme.dart
@@ -2,17 +2,22 @@ import 'package:flutter/material.dart';
 
 ThemeData appTheme() {
   return ThemeData(
-      textTheme: TextTheme(
-        title: TextStyle(color: Colors.green),
-        display1: TextStyle(color: Colors.yellow),
-        display2: TextStyle(color: Colors.red),
-        display3: TextStyle(color: Colors.cyanAccent),
-        display4: TextStyle(color: Colors.orange),
-        headline: TextStyle(color: Colors.deepPurpleAccent),
-        caption: TextStyle(color: Colors.brown),
-        overline: TextStyle(color: Colors.orangeAccent),
-        body1: TextStyle(color: Colors.black45),
-        body2: TextStyle(color: Colors.tealAccent),
-      ),
-      primarySwatch: Colors.brown);
+    textTheme: TextTheme(
+      title: TextStyle(color: Colors.green),
+      display1: TextStyle(color: Colors.yellow),
+      display2: TextStyle(color: Colors.red),
+      display3: TextStyle(color: Colors.cyanAccent),
+      display4: TextStyle(color: Colors.orange),
+      headline: TextStyle(color: Colors.deepPurpleAccent),
+      caption: TextStyle(color: Colors.brown),
+      overline: TextStyle(color: Colors.orangeAccent),
+      body1: TextStyle(color: Colors.black45),
+      body2: TextStyle(color: Colors.tealAccent),
+    ),
+    primarySwatch: Colors.blueGrey,
+    buttonTheme: ButtonThemeData(
+      buttonColor: Colors.black45,
+      textTheme: ButtonTextTheme.primary,
+    ),
+  );
 }
