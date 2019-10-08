aaebd7a60f240ec5688822e8206fc1764ef4171f
Aykut Asil <aykuttasil@gmail.com>
2019-10-08 23:43:53 +0300

    minor changes

--- lib/pages/ClipPath.dart
+++ lib/pages/ClipPath.dart
@@ -3,6 +3,14 @@ import 'package:flutter/material.dart';
 class ClipPathPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      width: double.infinity,
+      child: Column(
+        children: <Widget>[
+          Image.network("http://www.aykutasil.com/img/aykut.png"),
+          Text('Aykut Asil'),
+        ],
+      ),
+    );
   }
 }
