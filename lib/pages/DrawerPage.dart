cb31fa49e16096bcf01a3b8122a99bb26e298dbe
Aykut Asil <aykuttasil@gmail.com>
2019-10-18 23:50:04 +0300

    minor update

--- lib/pages/DrawerPage.dart
+++ lib/pages/DrawerPage.dart
@@ -3,6 +3,30 @@ import 'package:flutter/material.dart';
 class DrawerPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      drawer: buildDrawer(),
+      body: Container(
+        child: Text("Drawer Page"),
+      ),
+    );
+  }
+
+  Widget buildDrawer() {
+    return Drawer(
+      child: ListView(
+        children: <Widget>[
+          DrawerHeader(
+            child: Text("Drawer Header"),
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [Colors.cyan, Colors.orange],
+              ),
+            ),
+          ),
+          Text("Option 1"),
+          Text("Option 2"),
+        ],
+      ),
+    );
   }
 }
